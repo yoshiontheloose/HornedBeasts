4d6215a5e28048d0f04dd139a485cf25a201fe20
Clarissa P <clarissapamonicutt@gmail.com>
2021-07-14 23:54:50 -0700

    progress on lab02

--- src/Main.js
+++ src/Main.js
@@ -1,23 +1,25 @@
 import React from 'react';
 
 import HornedBeast from './HornedBeast.js';
-// import data from './data.json'
+import data from './data.json'
 
 
 class Main extends React.Component {
   render() {
+    // main component should pass props for the title, image and description to each horned beast element
+    
+    //forEach over the data and push into an array
+    // renders beast data from json file
+    let beastsToRender = [];
+
+
+      title={data.title}
+      imageUrl={data.image}
+      description={data.description}
+
     return (
       <main>
-      <HornedBeast
-      title="Jackalope"
-      imageUrl="https://www.grunge.com/img/gallery/jackalope-the-truth-behind-the-worlds-scariest-rabbit/intro-1563483930.jpg"
-      description="Spooky Bunny"
-      />
-      <HornedBeast
-      title="Tauros"
-      imageUrl="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/f/f7/Tauros.gif"
-      description="A wild Tauros appears"
-      />
+        {beastsToRender}
       </main>
     )
   }

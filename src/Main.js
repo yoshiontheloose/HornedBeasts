a17fbce35410ec30ff6b1a84a218d3395423e0a8
Clarissa P <clarissapamonicutt@gmail.com>
2021-07-14 22:01:50 -0700

    lab01 complete

--- src/Main.js
+++ src/Main.js
@@ -1,18 +1,26 @@
 import React from 'react';
 
 import HornedBeast from './HornedBeast.js';
+// import data from './data.json'
 
 
 class Main extends React.Component {
   render() {
     return (
-      <main
-        title="Jackalope"
-        imageUrl=""
-        description="Spooky bunny"
+      <main>
+      <HornedBeast
+      title="Jackalope"
+      imageUrl="https://www.grunge.com/img/gallery/jackalope-the-truth-behind-the-worlds-scariest-rabbit/intro-1563483930.jpg"
+      description="Spooky Bunny"
       />
+      <HornedBeast
+      title="Tauros"
+      imageUrl="https://oyster.ignimgs.com/mediawiki/apis.ign.com/pokemon-blue-version/f/f7/Tauros.gif"
+      description="A wild Tauros appears"
+      />
+      </main>
     )
   }
 }
 
-export default Main;
+export default Main;  

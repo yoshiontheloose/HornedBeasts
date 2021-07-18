a2b70b558919eacdd3b549d582db7b0e64c5b8d4
Clarissa P <clarissapamonicutt@gmail.com>
2021-07-17 23:54:25 -0700

    worked on lab03

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import './App.css';
 import Header from './Header.js';
 import Main from './Main.js';
 import Footer from './Footer.js';
+import data from './data.json'
 
 class App extends React.Component {
+
   render () {
     return (
       <>
         <Header />
-        <Main />
+        <Main data={data}/>
         <Footer />
       </>
     )
